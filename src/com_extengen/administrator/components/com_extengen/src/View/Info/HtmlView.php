a765c6d40ef780ad408683714ca2f25c4749c1b0
HermanPeeren <herman@yepr.nl>
2025-04-19 10:51:17 +0200

    update info

--- src/com_extengen/administrator/components/com_extengen/src/View/Info/HtmlView.php
+++ src/com_extengen/administrator/components/com_extengen/src/View/Info/HtmlView.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * @package     Extengen
+ <li>@package     Extengen
 
- * @subpackage  Extengen component
- * @version     0.8.0
+ <li>@subpackage  Extengen component
+ <li>@version     0.8.0
  *
- * @copyright   Copyright (C) Yepr, Herman Peeren, 2023. All rights reserved.
- * @license     GNU General Public License version 3 or later; see LICENSE.txt
+ <li>@copyright   Copyright (C) Yepr, Herman Peeren, 2023. All rights reserved.
+ <li>@license     GNU General Public License version 3 or later; see LICENSE.txt
  */
 
 namespace Yepr\Component\Extengen\Administrator\View\Info;
@@ -26,18 +26,18 @@ use Joomla\CMS\MVC\View\GenericDataException;
 
 
 /**
- * View class togive information about the Extension Generator project.
+ <li>View class togive information about the Extension Generator project.
  */
 class HtmlView extends BaseHtmlView
 {
 
 	/**
-	 * Method to display the view.
+	 <li>Method to display the view.
 	 *
-	 * @param   string  $tpl  A template file to load. [optional]
+	 <li>@param   string  $tpl  A template file to load. [optional]
 	 *
-	 * @return  void
-     * @throws Genericdataexception
+	 <li>@return  void
+     <li>@throws Genericdataexception
 	 */
 	public function display($tpl = null): void
 	{
@@ -45,104 +45,70 @@ class HtmlView extends BaseHtmlView
 		ExtengenHelper::addSubmenu('info');
 		$this->addToolbar();
 		$this->sidebar = \JHtmlSidebar::render();
+        
+        $info = <<<INFO
+        <h2>Extengen</h2>
+        <p>Extension generator as Joomla extension, model based on eJSL (JooMDD), with projectional editor</p>.
 
-		echo "<h2>Information about the Extension Generator project</h2>";
-		echo '<p>Planned featureswill be adjusted in the process.</p>';
-		echo '<h3>version 0.8.0</h3>';
-		echo '<p>Current version - under construction.</p>';
-		echo '<p>Plain component backend generated.</p>';
-		echo '<p>Todo:
-					<ul>
-							<li>form filters</li>
-							<li>editFields</li>
-					</ul>
-			  </p>';
-		echo '<h3>version 0.8.1</h3>';
-		echo '<p>Minimal Viable Product</p>';
-		echo '<p>features:
-					<ul>
-							<li>plain component frontend generated</li>
-							<li>fill entities & attributes dropdowns in extengen backend</li>
-							<li>install generated component in current Joomla 4 site (for testing)</li>
-					</ul>
-			  </p>';
-		echo '<h3>version 0.9.0</h3>';
-		echo '<p>features:
-					<ul>
-							<li>generator-generator: define and adjust generators</li>
-							<li>project forms generator: define and adjust project forms</li>
-					</ul>
-			  </p>';
-		echo '<h3>version 1.0.0</h3>';
-		echo '<p>features:
-					<ul>
-							<li>toggle Joomla core features in generated extension: categories, tags, versioning, workflow, pagination, custom fields, ordering, access control, language associations, routing/alias, action logs, finder</li>
-							<li>submenu for this component</li>
-							<li>toggle translations and choose translation service</li>
-							<li>override translations</li>
-							<li>add categories to projects and generators</li>
-							<li>add dashboard page type</li>
-							<li>update-site for extengen; update from github</li>
-					</ul>
-			  </p>';
-		echo '<h3>version 1.1.0</h3>';
-		echo '<p>features:
-					<ul>
-							<li>one-to-many relations</li>
-							<li>add page-type detail with indices (for the multiple files)</li>
-					</ul>
-			  </p>';
-		echo '<h3>version 1.2.0</h3>';
-		echo '<p>features:
-					<ul>
-							<li>save versions on Git(hub)</li>
-							<li>update extengen info page(s) from github instead of shipping with component</li>
-					</ul>
-			  </p>';
-		echo '<h3>version 1.3.0</h3>';
-		echo '<p>features:
-					<ul>
-							<li>generate modules</li>
-							<li>generate plugins</li>
-							<li>CLI and API applications</li>
-					</ul>
-			  </p>';
-		echo '<h3>version 1.4.0</h3>';
-		echo '<p>features:
-					<ul>
-							<li>import & export projects</li>
-							<li>migrate older version projects</li>
-					</ul>
-			  </p>';
-		echo '<h3>version 2.0.0</h3>';
-		echo '<p>features:
-					<ul>
-							<li>Initial WordPress generator</li>
-					</ul>
-			  </p>';
-		echo '<h3>version 2.1.0</h3>';
-		echo '<p>features:
-					<ul>
-							<li>Joomla4 + Doctrine generator</li>
-					</ul>
-			  </p>';
-		echo '<h3>version 2.2.0</h3>';
-		echo '<p>features:
-					<ul>
-							<li>possibility to use Event Sourcing in projects</li>
-							<li>Joomla4 + Prooph Event Sourcing generator</li>
-					</ul>
-			  </p>';
-		echo '<p>&nbsp</p>';
-		echo '<p>&nbsp</p>';
+    <p>First the JooMDD model was ported to Jetbrain's MPS (eJSL-MPS) and based on that structure this was turned into a 
+    Joomla extension, with HTML forms as input of the AST.</p>
+
+<h2>Information about the Extension Generator project</h2>
+<p>19-4-2025</p>
+
+<p>At the moment mainly working on getting the meta-level complete, using forms:</p>
+<ul>
+<li>Generator-generator: define and adjust generators.</li>
+<li>Project forms generator: define and adjust project forms.</li>
+
+<p>Still in the phase to get the first official release out. So be warned: the Extension Generator is still not production-ready.</p>
+
+<h3>version 0.9.0</h3>
+<p>If the model and the generator can both be created/edited, using forms, then the model and generator will be much easier
+to adjust. That's why I postponed all kinds of changes in model and generator until after this version.</p>
+
+
+<h3>version 1.0.0</h3>
+    <p>With this version basic Joomla components will be easy to make.</p>
+
+    <p>Features of model and generator, that will be added via the meta-level:</p>
+<ul>
+<li>toggle Joomla core features in generated extension: categories, tags, versioning, workflow, pagination, 
+custom fields, ordering, access control, language associations, routing/alias, action logs, finder</li>
+    <li>automatic junction table for n:n relations</li>
+    <li>submenu for this component</li>
+    <li>toggle translations and choose translation service; override translations</li>
+    <li>add dashboard page type</li>
+    <li>update-site for extengen; update from github</li>
+</ul>
+<h3>future version features</h3>
+<ul>
+    <li>import & export projects</li>
+    <li>migrate older version projects</li>
+    <li>save versions of the model and generator on Git(hub)</li>
+    <li>add page-type detail with indices (for the multiple files)</li>
+<li>update extengen info page(s) from github instead of shipping with component</li>
+        <li>generate Joomla modules</li>
+        <li>generate Joomla plugins</li>
+        <li>Joomla CLI and API applications</li>
+        <li>Initial WordPress generator</li>
+        <li>Joomla + Doctrine generator</li>
+        <li>possibility to use Event Sourcing in projects</li>
+        <li>Joomla + Prooph Event Sourcing generator</li>
+</ul>
+
+INFO;
+
+        echo $info;
+
 
 		//parent::display($tpl);
 	}
 
 	/**
-	 * Add the page title and toolbar.
+	 <li>Add the page title and toolbar.
 	 *
-	 * @return  void
+	 <li>@return  void
 	 */
 	protected function addToolbar()
 	{
